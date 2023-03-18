71f8bc03f73b08a579441b471e822f27d6cb3a19
Viacheslav Miroshko <ccctux@gmail.com>
2023-03-18 14:02:50 +0200

    fix(app): keeping typings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 export * from './random';
+
+declare global {
+  interface Array<T> {
+    random(): T;
+  }
+
+  interface Object {
+    randomKey(): string;
+    randomValue(): never;
+  }
+
+  interface Set<T> {
+    random(): T;
+  }
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,16 +1,1 @@
 export {};
-
-declare global {
-  interface Array<T> {
-    random(): T;
-  }
-
-  interface Object {
-    randomKey(): string;
-    randomValue(): never;
-  }
-
-  interface Set<T> {
-    random(): T;
-  }
-}
